8883e496365554ec816c9caa4d1ca250e4c46061
James Brooks <jamesb91@gmail.com>
2020-07-25 10:30:55 -0600

    utils and create account

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ function App() {
   return (
     <div>
       <img src={process.env.PUBLIC_URL + "/assets/images/logo/exercise_wars_webtitle.png"} alt="webtitle_logo"/>
+      
+
     </div>
   );
 }
