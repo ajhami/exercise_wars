ea6a930a30a97a9ab1938646d326fa2e9c8c33bc
Brent Peterson <brentp24@gmail.com>
2020-08-04 16:07:37 -0600

    working on posting workout

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,31 +3,26 @@ import React, { Component } from 'react';
 import FriendFeed from "../components/FriendFeed";
 import { Container, Row, Col } from "reactstrap";
 import FriendSuggestions from "../components/FriendSuggestions";
-<<<<<<< HEAD
 import axios from "axios";
-=======
 import NewWorkout from "../components/SaveWorkout";
->>>>>>> master
+
+
 
 class Home extends Component {
-
-
 
   render() {
     axios.get("/api/workouts")
       .then(function (response) {
         console.log(response);
       })
-    .catch (error => console.log(error))
+      .catch(error => console.log(error))
 
     return (
 
       <Container>
-<<<<<<< HEAD
         <Row>
-          <Col><div>
-
-          </div>
+          <Col>
+            <NewWorkout />
           </Col>
           <Col xs={5}>
             <h3> Friend Feed</h3>
@@ -44,25 +39,6 @@ class Home extends Component {
           </Col>
         </Row>
       </Container>
-=======
-  <Row>
-    <Col><NewWorkout/></Col>
-    <Col xs={5}>
-      <h3> Friend Feed</h3>
-        <FriendFeed />
-        </Col>
-    <Col>
-    <h3>Friend Suggestions</h3>
-    <FriendSuggestions 
-          id={1}
-          name="Jack Edwards"
-          location="Folsom, CA"
-          profileImg="test_profile_img.jpg"
-        />
-        </Col>
-  </Row>
-</Container>
->>>>>>> master
     );
   };
 };
