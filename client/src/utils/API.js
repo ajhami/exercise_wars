9349cee96c254a08cc438e838a550f365a09adab
Brent Peterson <brentp24@gmail.com>
2020-08-08 09:57:16 -0600

    reading some of the data from exercises

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,3 +1,28 @@
 import axios from 'axios';
 const APIURL = "some text"
 
+export default { 
+    fetchWorkouts:function() {
+        return axios
+        .get("/api/workouts")
+        .then(res => {
+            const workouts = res.data;
+            return workouts.map(workout => {
+                return {
+                    key: workout._id,
+                    id: workout._id,
+                    user: workout.user,
+                    title: workout.title,
+                    image: workout.image,
+                    description: workout.description,
+                    workoutType: workout.workoutType,
+                    likes: workout.likes,
+                    commments: workout.comments,
+                    exercises: workout.exercises
+
+                }
+            })
+        })
+        .catch(error => console.log(error))
+    }
+}
