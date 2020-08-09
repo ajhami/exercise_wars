d9e512ab9c1c61810b233f51665be6635cf6397a
Brent Peterson <brentp24@gmail.com>
2020-08-08 18:30:07 -0600

    NavBar

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,17 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 import { NavLink } from 'react-router-dom'
 import './style.css'
 // import { Card } from "reactstrap"
 
 function NavBar() {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const toggle = () => setDropdownOpen(prevState => !prevState);
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <NavLink className="navbar-brand"  to="/">
-        Home
+    <nav className="navbar navbar-expand-lg navbar-light bg-light"
+    style={{margin:"0px"}}>
+      <NavLink className="navbar-brand" to="/">
+      <img className="welcome_navbrand_img" src={process.env.PUBLIC_URL + "/assets/images/logo/apple-touch-icon.png"} alt="EW icon" 
+      style={{width: "30px", height: "30px"}}/>
       </NavLink>
       <div>
+        
         <ul className="navbar-nav">
-        <li className="nav-item">
+          <li className="nav-item">
             <NavLink
               to="/AddWorkout"
               className="nav-link"
@@ -22,49 +29,64 @@ function NavBar() {
               Add Workout
             </NavLink>
           </li>
-          <li className="nav-item">
-            <NavLink
-              to="/ChallengeFriends"
-              className="nav-link"
-              style={{ color: "darkGray" }}
-              activeStyle={{ color: "red" }}
-              exact
-            >
-              Challenge Friends
+          <li>
+            <Dropdown
+              isOpen={dropdownOpen} toggle={toggle}>
+              <DropdownToggle
+                style={{ color: "darkGray", backgroundColor: "#F8F9FA" }}
+                activeStyle={{ color: "red" }}
+                outline color="none"
+                caret>
+                Challenges
+        </DropdownToggle>
+              <DropdownMenu>
+                <DropdownItem>
+                  <NavLink
+                    to="/ChallengeFriends"
+                    className="nav-link"
+                    style={{ color: "darkGray" }}
+                    activeStyle={{ color: "red" }}
+                    exact
+                  >
+                    Challenge Friends
             </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink
-              to="/GlobalChallenge"
-              className="nav-link"
-              style={{ color: "darkGray" }}
-              activeStyle={{ color: "red" }}
-              exact
-            >
-              Global Challenge
+                </DropdownItem>
+
+                <DropdownItem>
+                  <NavLink
+                    to="/GlobalChallenge"
+                    className="nav-link"
+                    style={{ color: "darkGray" }}
+                    activeStyle={{ color: "red" }}
+                    exact
+                  >
+                    Global Challenge
             </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink
-              to="/LocalChallenge"
-              className="nav-link"
-              style={{ color: "darkGray" }}
-              activeStyle={{ color: "red" }}
-              exact
-            >
-              Local Challenge
+                </DropdownItem>
+                <DropdownItem>
+                  <NavLink
+                    to="/LocalChallenge"
+                    className="nav-link"
+                    style={{ color: "darkGray" }}
+                    activeStyle={{ color: "red" }}
+                    exact
+                  >
+                    Local Challenge
             </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink
-              to="/SoloChallenge"
-              className="nav-link"
-              style={{ color: "darkGray" }}
-              activeStyle={{ color: "red" }}
-              exact
-            >
-              Solo Challenge
+                </DropdownItem>
+                <DropdownItem>
+                  <NavLink
+                    to="/SoloChallenge"
+                    className="nav-link"
+                    style={{ color: "darkGray" }}
+                    activeStyle={{ color: "red" }}
+                    exact
+                  >
+                    Solo Challenge
             </NavLink>
+                </DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
           </li>
         </ul>
       </div>
--- client/src/pages/SoloChallenge.js
+++ client/src/pages/SoloChallenge.js
@@ -10,16 +10,16 @@ class SoloChallenge extends Component {
 
   render() {
     return (
+      <div>
+      <NavBar />
+
 
       <div className="container" style={{ marginBottom: "25px", border: "1px solid black", maxWidth: "355px" }}>
-
-
         <div className="row" style={{ border: "1px solid black" }}>
           <div className="img" style={{ height: "250px", width: "100%" }} >
             <img className="img soloImage" src={process.env.PUBLIC_URL + "/assets/images/logo/second_pushup.jpg"} alt="Card image cap" >
             </img>
           </div>
-
           <div className="row">
             <div className="challenge" style={{ marginLeft: "20%" }}>
               <h3> Minute Challenge</h3>
@@ -43,6 +43,8 @@ class SoloChallenge extends Component {
             </div>
           </div>
         </div >
+      </div>
+      <Footer />
       </div>
     );
   };
