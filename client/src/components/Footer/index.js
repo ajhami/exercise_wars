518bb4a3512f2cb4471e2ac3254893fedd8e3fa5
Scott Morrison <scottmorr75@gmail.com>
2020-08-09 11:40:41 -0600

    footer work

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -7,7 +7,8 @@ function Footer() {
         <div className="footer" style={{ border: "1px solid black" }}>
             <div className="row">
                 <div className="col-sm">
-                    
+                    <img className="welcome_navbrand_img" src={process.env.PUBLIC_URL + "/assets/images/logo/apple-touch-icon.png"} alt="EW icon"
+                        style={{ width: "100px", height: "100px", marginLeft: "35px" }} />
                 </div>
 
 
@@ -18,19 +19,19 @@ function Footer() {
                     <h6>Corporate Office</h6>
                     <h6>345 Exercise Ln.</h6>
                     <h6>Salt Lake City, Ut  84109</h6>
-
                 </div>
-                <div className="col-sm">
 
-                    <div className="footerLinks">
-                        <a href="mailto:hege@example.com">exercisewars@gmail.com</a><br></br>
+                <div className="col-sm">
+                    <div className="footer">
+                        <p>Email:</p>
+                        <div className="footerLinks">
+                            <a href="mailto:hege@example.com">exercisewars@gmail.com</a>
+                        </div>
                     </div>
                 </div>
-
                 <div className="col-sm">
-                    <a href="https://secure.actblue.com/donate/biden2020_fr_web?refcode=web-homepage-nav" className="btn btn-secondary btn-lg" role="button" aria-disabled="true">Donate</a>
+                    <a href="https://secure.humanesociety.org/site/SPageServer?pagename=splash&s_src=ad_bing_nonbranded_donate%20to%20animals&msclkid=89e8385453cc1d3227a6676467517204&utm_source=bing&utm_medium=cpc&utm_campaign=Non%20Branded%20-%20Animal%20Charities&utm_term=donate%20to%20animals&utm_content=Animal%20Charities%20-%20Donations" className="btn btn-secondary btn-lg" role="button" aria-disabled="true" style={{ marginTop: "30px", marginLeft: "75px" }}>Donate</a>
                 </div>
-
             </div>
         </div>
     )
