c2dd17b5e18521f82025d8f69cb37624c82af354
Scott Morrison <scottmorr75@gmail.com>
2020-08-10 21:27:23 -0600

    responsive footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -4,26 +4,27 @@ import './style.css'
 function Footer() {
     return (
 
-        <div className="footer" style={{ height: "120px", backgroundColor:"grey"}}>
+        <div className="footer" style={{ height: "auto", backgroundColor:"grey"}}>
             <div className="row">
-                <div className="col-sm">
+               
+                <div className="col-sm" style = {{width: "60%"}}>
                     <img className="img" id="minute" src={process.env.PUBLIC_URL + "/assets/images/logo/exercise_wars_title.png"} alt="Card image cap"
-                        style={{ height: "120px", marginLeft: "85px" }}>
+                        style={{ height: "140px", marginLeft: "95px" }}>
                     </img>
                 </div>
 
-                <div className="col-sm" style={{ marginTop: "5px", marginLeft: "75px" }}>
-                    <div className="info" style={{ marginLeft: "125px" }}>
-                        <h5>Contact Exercise Wars:</h5>
-                        <h6>Corporate Office</h6>
-                        <h6>345 Exercise Ln.</h6>
-                        <h6>Salt Lake City, Ut  84109</h6>
+                <div className="col-sm" style={{ marginTop: "5px"}}>
+                    <div className="info" style={{ marginLeft: "125px", width: "45%", marginBottom: "15px" }}>
+                        <h6>Contact:</h6>
+                        <h7>Corporate Office</h7>
+                        <h7>345 Exercise Ln.</h7>
+                        <h7>Salt Lake City, Ut  84109</h7>
                     </div>
                 </div>
 
 
-                <div className="col-sm">
-                    <div className="email" style={{ marginTop: "5px", marginLeft: "65px"}}>
+                <div className="col-sm" >
+                    <div className="email" style={{ marginTop: "5px", marginLeft: "90px", width: "80%"}}>
                         <h5>Email:</h5>
                         <div className="link" style = {{fontSize: "25px"}}>
                         <a href="mailto:hege@example.com">exercisewars@gmail.com</a>
@@ -31,9 +32,9 @@ function Footer() {
                     </div>
                 </div>
 
-                <div className="col-sm">
+                <div className="col-sm" style = {{}}>
 
-                    <a href="#" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true" style = {{marginTop: "25px"}}>Donate</a>
+                    <a href="https://www.theroadhome.org/give/in-kind-donations/" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true" style = {{marginTop: "25px", marginLeft:"125px"}}>Donate</a>
                 </div>
 
 
