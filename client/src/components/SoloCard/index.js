7f00058ea69883faa52834714b378bc4ca023d83
Scott Morrison <scottmorr75@gmail.com>
2020-08-11 19:09:27 -0600

    styling page

--- client/src/components/SoloCard/index.js
+++ client/src/components/SoloCard/index.js
@@ -2,21 +2,29 @@ import React, { Component } from 'react';
 import ChallengeForm from "../ChallengeForm"
 import ChallengeDropDown from "../ChallengeDropDown"
 import NavBar from "../NavBar"
+import Footer from "../Footer"
+import "./style.css";
+
 function SoloCard(props) {
     return (
         <div>
             < NavBar />
 
-            <div className="card" style={{ height: "480px", backgroundColor: "grey", width: "25%", border: "1px solid black", marginLeft:"15px" }}>
-                <div className="row">
+            <div className="row">
+                <div class="col-sm">
+                    <div className="img">
+                        <img className="img" src={process.env.PUBLIC_URL + "/assets/images/logo/celebrate2.png"} alt="Card image cap" style={{ height: "480px", width: "90%", marginLeft: "15px", marginTop: "15px" }}                    >
+                        </img>
+                    </div>
+                </div>
+                <div className="card" style={{ height: "480px", backgroundColor: "grey", width: "25%", border: "1px solid black", marginRight: "40px", marginBottom: "35px" }}>
                     <div className="col-sm" style={{ marginLeft: "125px", marginBottom: "25px", marginTop: "15px" }} >
                         <h2> {props.name}</h2>
                         <h4></h4>
                         <ChallengeForm />
                     </div>
-                </div>
-                <div className="row">
-                    <div className="col-sm" style={{ marginBottom: "25px", marginLeft: "15px" }}>
+                   
+                    <div className="col-sm" style={{ marginBottom: "95px", marginLeft: "11%" }}>
                         <div className="img" >
                             <img className="img" src={props.imgsrc} alt="Card image cap" >
 
@@ -24,17 +32,23 @@ function SoloCard(props) {
                         </div>
 
                     </div>
-                </div>
-                <div className="row">
+                   
 
-                    <div className="col-sm" style={{ marginLeft: "125px", marginBottom: "25px" }}       >
+                    <div className="col-sm" style={{ marginLeft: "125px", marginTop: "15px" }}       >
                         <h3>Succesful?</h3>
                         < ChallengeDropDown />
                     </div>
                 </div>
+                <div class="col-sm">
+                    <div class="col-sm">
+                        <div className="img">
+                            <img className="img" src={process.env.PUBLIC_URL + "/assets/images/logo/celebrate3.png"} alt="Card image cap" style={{ height: "480px", width: "100%", marginBottom: "15px", marginTop: "15px" }}                    >
+                            </img>
+                        </div>
+                    </div>
+                </div>
             </div>
-
-
+            < Footer />
 
         </div>
     )
