87b590f6a2c11a3162fc58d9496c25db9aaaaebf
Scott Morrison <scottmorr75@gmail.com>
2020-08-11 22:20:54 -0600

    styling

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -4,7 +4,7 @@ import './style.css'
 function Footer() {
     return (
 
-        <div className="footer" style={{  backgroundColor:"grey", marginTop: "15px"}}>
+        <div className="footer" style={{  backgroundColor:"grey"}}>
             <div className="row">
                
                 <div className="col-sm" style = {{width: "60%"}}>
@@ -32,8 +32,9 @@ function Footer() {
                     </div>
                 </div>
 
-                <div className="col-sm" style = {{}}>
-                    <a href="https://www.theroadhome.org/give/in-kind-donations/" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true" style = {{marginTop: "25px", marginLeft:"30%", marginBottom: "15px"}}>Donate</a>
+                <div className="col-sm">
+
+                    <a href="https://www.theroadhome.org/give/in-kind-donations/" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true" style = {{marginTop: "25px", marginLeft:"30%", marginBottom: "15px" , backgroundColor: "#333333"}}>Donate</a>
                 </div>
 
 
