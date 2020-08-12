2696df5478fddcff296b95ff055589cb20408030
James Brooks <jamesb91@gmail.com>
2020-08-12 00:36:12 -0600

    stylings for minute challenge

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -8,26 +8,26 @@ function Footer() {
             <div className="row">
                
                 <div className="col-sm" style = {{width: "60%"}}>
-                    <img className="img" id="minute" src={process.env.PUBLIC_URL + "/assets/images/logo/exercise_wars_title.png"} alt="Card image cap"
+                    <img className="img" id="minute" src={process.env.PUBLIC_URL + "/assets/images/logo/exercise_wars_title.png"} alt="Card cap"
                         style={{ height: "140px", marginLeft: "80px" }}>
                     </img>
                 </div>
 
                 <div className="col-sm" style={{ marginTop: "0px"}}>
                     <div className="info" style={{ marginLeft: "125px", width: "45%", marginBottom: "15px" }}>
-                        <h6>Contact:</h6>
-                        <h7>Corporate Office</h7>
-                        <h7>345 Exercise Ln.</h7>
-                        <h7>Salt Lake City, Ut  84109</h7>
+                        <h5>Contact:</h5>
+                        <p>Corporate Office</p>
+                        <p>345 Exercise Ln.</p>
+                        <p>Salt Lake City, Ut  84109</p>
                     </div>
                 </div>
 
 
                 <div className="col-sm" >
-                    <div className="email" style={{ marginTop: "5px", marginLeft: "55px", width: "100%"}}>
-                        <h5>Email:</h5>
+                    <div className="email" style={{ marginTop: "5px", marginLeft: "auto", width: "100%", marginRight:"auto"}}>
+                        <h5 className="emailheader">Email:</h5>
                         <div className="link" style = {{fontSize: "25px", marginLeft: "25px" }}>
-                        <a href="mailto:hege@example.com">ewars@gmail.com</a>
+                        <a className="emailanchor" href="mailto:hege@example.com">ewars@gmail.com</a>
                         </div>
                     </div>
                 </div>
--- client/src/components/SoloCard/index.js
+++ client/src/components/SoloCard/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ChallengeForm from "../ChallengeForm"
 import ChallengeDropDown from "../ChallengeDropDown"
 import NavBar from "../NavBar"
@@ -9,40 +9,41 @@ function SoloCard(props) {
     return (
         <div>
             < NavBar />
-
-            <div className="row">
-                <div class="col-sm">
-                    <div className="img">
-                        <img className="img" src={process.env.PUBLIC_URL + "/assets/images/logo/celebrate2.png"} alt="Card image cap" style={{ height: "495px", width: "100%", marginTop: "15px" }}                    >
-                        </img>
-                    </div>
-                </div>
-
-                <div className="card" style={{ height: "495px", backgroundColor: "grey", width: "25%", border: "1px solid black", marginRight: "40px", marginLeft: "2%" }}>
-                    <div className="col-sm" style={{ marginLeft: "35%", marginBottom: "25px", marginTop: "15px"}} >
-                        <h2> {props.name}</h2>
-                        <ChallengeForm />
-                    </div>
-                   
-                        <div className="img" >
-                            <img className="img" src={props.imgsrc} alt="Card image cap" style = {{width:"100%", height:"250px", marginBottom: "10%"}} >
-
-                            </img>
-                        </div>
-                   
-
-                    <div className="dropdown" style={{ marginLeft: "35%", marginBottom: "7%" }}       >
-                        <h3>Succesful?</h3>
-                        < ChallengeDropDown />
-                    </div>
-
-                </div>
+            <div className="container">
+                <div className="row">
                     <div class="col-sm">
                         <div className="img">
-                            <img className="img" src={process.env.PUBLIC_URL + "/assets/images/logo/celebrate3.png"} alt="Card image cap" style={{ height: "495px", width: "100%", marginBottom: "10px", marginTop:"4%" }}                    >
+                            <img className="img" src={process.env.PUBLIC_URL + "/assets/images/logo/celebrate2.png"} alt="Card cap" style={{ height: "495px", width: "100%", marginTop: "15px" }}                    >
                             </img>
                         </div>
                     </div>
+
+                    <div className="card" style={{ display: "block", marginRight: "auto", border: "0px", marginLeft: "auto", height: "495px", backgroundColor: "grey", width: "25%", marginTop: "15px" }}>
+                        <div className="col-sm" style={{ marginLeft: "auto", marginRight: "auto", marginBottom: "25px", marginTop: "15px" }} >
+                           <h2 className = "cardheader"> {props.name}</h2>
+                            <ChallengeForm />
+                        </div>
+
+                        <div className="img" >
+                            <img className="img" src={props.imgsrc} alt="Card cap" style={{ width: "100%", height: "250px", marginBottom: "15px" }} >
+
+                            </img>
+                        </div>
+
+
+                        <div className="drop-down" style={{ marginLeft: "auto", marginRight: "auto", marginBottom: "15px" }}       >
+                            <h3 className= "h3dropdown">Complete</h3>
+                            < ChallengeDropDown />
+                        </div>
+
+                    </div>
+                    <div class="col-sm">
+                        <div className="img">
+                            <img className="img" src={process.env.PUBLIC_URL + "/assets/images/logo/celebrate3.png"} alt="Card cap" style={{ height: "495px", width: "100%", marginBottom: "10px", marginTop: "15px" }}                    >
+                            </img>
+                        </div>
+                    </div>
+                </div>
             </div>
             < Footer />
 
