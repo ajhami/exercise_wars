1483eae08d2016def6b485142eb6531f416490ed
Scott Morrison <scottmorr75@gmail.com>
2020-08-12 13:13:44 -0600

    minute challenge

--- client/src/components/MinuteChallenge/index.js
+++ client/src/components/MinuteChallenge/index.js
@@ -4,12 +4,12 @@ import './style.css'
 
 function MinuteChallenge() {
     return (
-        <div className="card" style={{ height: "450px", backgroundColor: "aquamarine", width: "25%" }}>
+        <div className="card" style={{ height: "470px", backgroundColor: "grey", width: "100%" }}>
             <div className="img">
                 <img className="img" id="minute" src={process.env.PUBLIC_URL + "/assets/images/logo/pushup.png"} alt="Card image cap" >
                 </img>
 
-                <a href="/SoloChallenge" className="btn btn-primary btn-lg active" role="button" aria-pressed="true" style = {{marginTop: "25px", marginLeft: "23%"}}>Minute Challenge</a>
+                <a href="/SoloChallenge" className="btn btn-primary btn-lg active" role="button" aria-pressed="true" style = {{marginTop: "45px", marginLeft: "23%",backgroundColor: "#333333"}}>Minute Challenge</a>
             </div>
         </div>
     )
