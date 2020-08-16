d0faea616ee36102cb3aed2d2d7e037b9d2744d2
Alex Hamilton <alexhami916@gmail.com>
2020-08-16 10:02:11 -0600

    quick changes

--- client/src/components/FriendFeed/index.js
+++ client/src/components/FriendFeed/index.js
@@ -54,20 +54,20 @@ const FriendFeed = (props) => {
                         return (
                             <Workout
                                 key={i++}
-                                id={workout?.id}
-                                user={workout?.user}
-                                date={workout?.date}
-                                title={workout?.title}
-                                time={workout?.time}
-                                timeHours={workout?.timeHours}
-                                timeMinutes={workout?.timeMinutes}
-                                timeSeconds={workout?.timeSeconds}
-                                image={workout?.image}
-                                description={workout?.description}
-                                workoutType={workout?.workoutType}
-                                likes={workout?.likes}
-                                comments={workout?.comments}
-                                exercises={workout?.exercises}
+                                id={workout.id}
+                                user={workout.user}
+                                date={workout.date}
+                                title={workout.title}
+                                time={workout.time}
+                                timeHours={workout.timeHours}
+                                timeMinutes={workout.timeMinutes}
+                                timeSeconds={workout.timeSeconds}
+                                image={workout.image}
+                                description={workout.description}
+                                workoutType={workout.workoutType}
+                                likes={workout.likes}
+                                comments={workout.comments}
+                                exercises={workout.exercises}
                                 clickDoWorkout={clickDoWorkout}
                             />
                         )
