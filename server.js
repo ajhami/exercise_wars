7a2c3f6135b22f118d99efeb99dc4de5d1d2cfb8
Alex Hamilton <alexhami916@gmail.com>
2020-09-07 11:37:58 -0600

    fix for heroku

--- server.js
+++ server.js
@@ -33,10 +33,10 @@ app.use(express.static("public"));
 
 if(process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/client/build")));
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.join(__dirname + "/client/build/index.html"));
-  // });
-  app.get("/*", (req, res) => {
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  });
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname + "/client/build/index.html"));
   });
 }
