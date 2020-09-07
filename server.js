cacbdebb5db972962a435cfe415d849fb947e45e
Alex Hamilton <alexhami916@gmail.com>
2020-09-07 11:55:12 -0600

    tweek for heroku

--- server.js
+++ server.js
@@ -33,12 +33,12 @@ app.use(express.static("public"));
 
 if(process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/client/build")));
-  app.use("/", (req, res) => {
+  app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname + "/client/build/index.html"));
   });
-  // app.get("*", (req, res) => {
-  //   res.sendFile(path.join(__dirname + "/client/build/index.html"));
-  // });
+  app.use("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  });
 }
 
 
