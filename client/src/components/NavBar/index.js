1fbd086aedcd4f7ec2e6b3b63c6e1dc5e675556a
Brent Peterson <brentp24@gmail.com>
2020-09-28 22:05:56 -0600

    signoutIcon

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -1,108 +1,103 @@
 import React, { useState } from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom';
+import { Navbar, Nav } from "reactstrap";
 import './style.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
 function NavBar() {
-
-
-
-
   return (
-    <div>
-      <nav className="navbar navbar-expand navbar-light bg-light"
-        style={{ margin: "0px" }}>
-        <NavLink className="navbar-brand"
-          to="/Home"
-          style={{ paddingRight: "20px" }}
-          activestyle={{ color: "red" }}
-          exact>
-          <img className="welcome_navbrand_img mr-1" src={process.env.PUBLIC_URL + "/assets/images/logo/apple-touch-icon.png"} alt="EW icon"
-            style={{ width: "30px", height: "30px" }} />
-          <span className="d-none d-sm-block">Home</span>
-        </NavLink>
-        <div>
+    <Navbar className="navbar navbar-expand navbar-light bg-light"
+      style={{ margin: "0px" }}>
+        
 
-          <ul className="navbar-nav">
+      <NavLink className="navbar-brand"
+        to="/Home"
+        style={{ paddingRight: "20px" }}
+        activestyle={{ color: "red" }}
+        exact>
+        <img className="welcome_navbrand_img mr-1" src={process.env.PUBLIC_URL + "/assets/images/logo/apple-touch-icon.png"} alt="EW icon"
+          style={{ width: "30px", height: "30px" }} />
+        <span className="d-none d-sm-block">Home</span>
+      </NavLink>
 
-            <li className="nav-item">
-              <NavLink
-                to="/SearchProfiles"
-                className="nav-link"
-                style={{ color: "darkGray", paddingBottom: "0px" }}
-                activeStyle={{ color: "red" }}
-                exact
-              >
-                <FontAwesomeIcon icon="users" />
-                <span className="d-block d-sm-none"></span>
-                <span className="d-none d-sm-inline"><i className="d-none d-sm-inline mr-1"></i>Profiles</span>
+        <Nav className="mr-auto">
 
 
-              </NavLink>
-            </li>
-
-            <li className="nav-item">
-              <NavLink
-                to="/FeedPage"
-                className="nav-link"
-                style={{ color: "darkGray" }}
-                activeStyle={{ color: "red" }}
-                exact
-              >
-                <FontAwesomeIcon icon="dumbbell" />
-                <span className="d-block d-sm-none"></span>
-                <span className="d-none d-sm-inline"><i className="d-none d-sm-inline mr-1"></i>Feed</span>
-              </NavLink>
-            </li>
-
-            <li className="nav-item">
-              <NavLink
-                to="/AddWorkout"
-                className="nav-link"
-                style={{ color: "darkGray" }}
-                activeStyle={{ color: "red" }}
-                exact
-              >
-                <FontAwesomeIcon icon="plus" />
-                <span className="d-block d-sm-none"></span>
-                <span className="d-none d-sm-inline"><i className="d-none d-sm-inline mr-1"></i>Add</span>
-              </NavLink>
-            </li>
-
-            <li className="nav-item">
-            <NavLink
-                      to="/SoloChallenge"
-                      className="nav-link"
-                      style={{ color: "darkGray" }}
-                      activeStyle={{ color: "red" }}
-                      exact
-                    >
-                      <FontAwesomeIcon icon="stopwatch" />
-                      <span className="d-block d-sm-none"></span>
-                      <span className="d-none d-sm-inline"><i className="d-none d-sm-inline mr-1"></i>Challenges</span>
-
-                    </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/Logout"
-                className="nav-link"
-                style={{ color: "darkGray", paddingBottom: "0px" }}
-                activeStyle={{ color: "red" }}
-                exact
-              >
-                <span className="d-inline">Logout</span>
+          <NavLink
+            to="/SearchProfiles"
+            className="nav-link"
+            style={{ color: "darkGray", paddingBottom: "0px" }}
+            activeStyle={{ color: "red" }}
+            exact
+          >
+            <FontAwesomeIcon icon="users" />
+            <span className="d-block d-sm-none"></span>
+            <span className="d-none d-sm-inline"><i className="d-none d-sm-inline mr-1"></i>Profiles</span>
 
 
-              </NavLink>
+          </NavLink>
 
 
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </div>
+
+          <NavLink
+            to="/FeedPage"
+            className="nav-link"
+            style={{ color: "darkGray" }}
+            activeStyle={{ color: "red" }}
+            exact
+          >
+            <FontAwesomeIcon icon="dumbbell" />
+            <span className="d-block d-sm-none"></span>
+            <span className="d-none d-sm-inline"><i className="d-none d-sm-inline mr-1"></i>Feed</span>
+          </NavLink>
+
+
+
+          <NavLink
+            to="/AddWorkout"
+            className="nav-link"
+            style={{ color: "darkGray" }}
+            activeStyle={{ color: "red" }}
+            exact
+          >
+            <FontAwesomeIcon icon="plus" />
+            <span className="d-block d-sm-none"></span>
+            <span className="d-none d-sm-inline"><i className="d-none d-sm-inline mr-1"></i>Add</span>
+          </NavLink>
+
+
+
+          <NavLink
+            to="/SoloChallenge"
+            className="nav-link"
+            style={{ color: "darkGray" }}
+            activeStyle={{ color: "red" }}
+            exact
+          >
+            <FontAwesomeIcon icon="stopwatch" />
+            <span className="d-block d-sm-none"></span>
+            <span className="d-none d-sm-inline"><i className="d-none d-sm-inline mr-1"></i>Challenges</span>
+
+          </NavLink>
+
+        </Nav>
+
+        <NavLink
+            to="/Logout"
+            className="nav-link"
+            style={{ color: "darkGray", paddingBottom: "0px" }}
+            activeStyle={{ color: "red" }}
+            exact
+          >
+            <FontAwesomeIcon icon={['fas', 'sign-out-alt']} />
+            <span className="d-block d-sm-none"></span>
+            <span className="d-none d-sm-inline"><i className="d-none d-sm-inline mr-1"></i>Logout</span>
+
+
+          </NavLink>
+
+    </Navbar>
   );
 }
 
