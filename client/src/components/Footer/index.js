929398b2f37b6634aaa74f89e43662db33d29de1
Brent Peterson <brentp24@gmail.com>
2020-11-17 21:59:55 -0700

    footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -4,25 +4,21 @@ import { Container, Row, Col } from "reactstrap";
 function Footer() {
     return (
         <div style={{ backgroundColor: "#CED4DA" }}>
-            <Container className="footer">
-                
+            <Container className="footer" >
+
                 <Row>
                     <img className="logoImage" src={process.env.PUBLIC_URL + "/assets/images/logo/exercise_wars_title.png"} alt="Card cap"
                     >
                     </img>
                 </Row>
                 <Row>
-                    <Col md={6} style={{marginBottom: "15px"}}>
-                        <div className="contactLabel">Contact us:</div><a className="btn btn-secondary btn-lg active" role="button" aria-pressed="true" style={{ width: "100px", backgroundColor: "#343A40" }} href="mailto:info@exercisewars.com">Email</a>
-                    </Col>
-                    <Col md={6} style={{marginBottom: "15px"}}>
-                    <div className="contactLabel">Donate to our selected cause: </div>
-                    <a href="https://www.theroadhome.org/give/in-kind-donations/" className="btn btn-secondary btn-lg active" role="button" aria-pressed="true" style={{width: "100px", backgroundColor: "#343A40" }}>Donate</a>
-                    </Col>
+                    <Col>
+                        <div className="contactLabel"><a href="mailto:info@exercisewars.com">Contact us</a></div>
+                        </Col>
                 </Row>
                 <Row>
-                <span id="siteseal"><script async type="text/javascript" src="https://seal.godaddy.com/getSeal?sealID=xa9hsUQ4db6shEL68tt4sm9jOmufZ3IVMrH4OiXgJ3aziz0cMd0H20w2wVyW"></script></span>
-                    
+                    <span id="siteseal"><script async type="text/javascript" src="https://seal.godaddy.com/getSeal?sealID=xa9hsUQ4db6shEL68tt4sm9jOmufZ3IVMrH4OiXgJ3aziz0cMd0H20w2wVyW"></script></span>
+
                 </Row>
             </Container>
         </div>
