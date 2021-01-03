f05f1b1ce28d6c3ba28a7b35892c88e60efb9f3c
Brent Peterson <brentp24@gmail.com>
2021-01-03 08:10:22 -0700

    autocomplete

--- client/src/components/Form/SubForm.js
+++ client/src/components/Form/SubForm.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Button, Container, Table } from "reactstrap";
 import "./table.css";
+import Autocomplete from 'react-autocomplete'
 
 function SubForm(props) {
     const handleChange = idx => e => {
@@ -13,6 +14,61 @@ function SubForm(props) {
         };
         props.onExerciseUpdate(rows)
     };
+
+    var exercises = [
+        { value: "Air squat" },
+        { value: "Arm Circles" },
+        { value: "Bear Crawl" },
+        { value: "Bent-over row" },
+        { value: "Bicep curl" },
+        { value: "Bicycle" },
+        { value: "Bike" },
+        { value: "Calf raise" },
+        { value: "Chin-up" },
+        { value: "Crunch" },
+        { value: "Burpee" },
+        { value: 'Dead lift' },
+        { value: 'Diamond push-up' },
+        { value: 'Dip' },
+        { value: 'Dolphin push-up' },
+        { value: 'Donkey kick' },
+        { value: 'Flutter kick' },
+        { value: 'Fly' },
+        { value: 'Good Morning' },
+        { value: 'Handstand push-up' },
+        { value: 'Inchworm' },
+        { value: 'Karaoke' },
+        { value: 'L seat' },
+        { value: 'Leg curl' },
+        { value: 'Lunge' },
+        { value: 'Lying triceps extension' },
+        { value: 'Mountain climber' },
+        { value: 'Muscle-up' },
+        { value: 'Overhead press' },
+        { value: 'Pistol squat' },
+        { value: 'Plank' },
+        { value: 'Plyometric push-up' },
+        { value: 'Power clean' },
+        { value: 'Pull-up' },
+        { value: 'Push-up' },
+        { value: 'Reverse fly' },
+        { value: 'Run' },
+        { value: 'Russian Twist' },
+        { value: 'Shoulder bridge' },
+        { value: 'Side plank' },
+        { value: 'Single leg dead lift' },
+        { value: 'Sit-up' },
+        { value: 'Snatch' },
+        { value: 'Step-up' },
+        { value: 'Superman' },
+        { value: 'Swim' },
+        { value: 'tricep dips' },
+        { value: 'Tuck jump' },
+        { value: 'Upright row' },
+        { value: 'Walk' },
+        { value: 'Wall sit' }
+    ]
+console.log(value)
     return (
         <Container style={{ margin: "5px", }}>
             <div>
@@ -29,14 +85,25 @@ function SubForm(props) {
                                             <tr id="addr0" key={idx}>
                                                 <td>{idx + 1}</td>
                                                 <td style={{ width: "125px", }}>
-                                                    <input
+
+                                                    <Autocomplete
+                                                        getItemValue={(item) => item.value}
+                                                        items={exercises}
+                                                        renderItem={(item, isHighlighted) =>
+                                                            <div style={{ background: isHighlighted ? 'lightgray' : 'white' }}>
+                                                                {item.value}
+                                                            </div>
+                                                        }
                                                         type="text"
                                                         name="exerciseName"
                                                         placeholder="exercise"
-                                                        value={item.exerciseName}
-                                                        onChange={handleChange(idx)}
+                                                        value={item.value}
+                                                        onChange={(e) => item.value = e.target.value}
+                                                        onSelect={(val) => item.value = val}
                                                         className="form-control"
                                                     />
+
+
                                                 </td>
                                                 <td style={{ width: "80px", }}>
                                                     <input
